cd86a1da30ac6ea5b390307f915bbded8552e787
DominicJamesWhite <dominicjameswhite@gmail.com>
2025-08-09 17:05:43 +0200

    Whoops that broke a bunch of stuff

--- src/server/shared/DataStores/DBCStructure.h
+++ src/server/shared/DataStores/DBCStructure.h
@@ -1916,7 +1916,7 @@ struct SummonPropertiesEntry
     uint32  Flags;                                          // 5
 };
 
-#define MAX_TALENT_RANK 1
+#define MAX_TALENT_RANK 5
 #define MAX_PET_TALENT_RANK 3                               // use in calculations, expected <= MAX_TALENT_RANK
 #define MAX_TALENT_TABS 3
 
